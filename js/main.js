08952e1ab1deea9e7cb44598b678689296e95a85
genarogg <gena_gonzalez15@hotmail.com>
2019-09-15 20:57:28 -0400

    validation

--- js/main.js
+++ js/main.js
@@ -2,3 +2,20 @@ var menu = document.querySelector(".menu");
 var burgerButton = document.querySelector("#burger-menu");
 
 var ipad = window.matchMedia("screen and (max-width: 767px)");
+
+ipad.addListener(validation);
+
+function validation(event){
+    if(event.matches){
+         burgerButton.addEventListener("click", hideShow);
+    }
+
+    else{
+        burgerButton.removeEventListener("click", hideShow);
+
+    }
+    
+}
+
+
+
