2565ec36c64b0e17ef11b2ed700fdbcbf88373f6
genarogg <gena_gonzalez15@hotmail.com>
2019-09-18 23:09:25 -0400

    fix

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 var menu = document.querySelector(".menu");
 var burgerButton = document.querySelector("#burger-menu");
 
+
 var ipad = window.matchMedia("screen and (max-width: 767px)");
 
 ipad.addListener(validation);
 
 function validation(event){
     if(event.matches){
-         burgerButton.addEventListener("click", hideShow);
+        burgerButton.addEventListener("click", hideShow);
     }
 
     else{
@@ -23,8 +24,8 @@ function hideShow(){
 
     if(menu.classList.contains('is-active')){
         menu.classList.remove("is-active");
-    } 
-    else{ 
+    }
+    else{
         menu.classList.add("is-active");
     }
 }
